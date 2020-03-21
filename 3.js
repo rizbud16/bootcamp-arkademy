8429d91ce85480c646ce23ff499b943e4a11379a
Rizki Budi <rizbud20@gmail.com>
2020-03-21 15:14:29 +0700

    Add 3.js

---
+++ 3.js
@@ -0,0 +1,15 @@
+function triangle(num){
+  print = ""
+  if(typeof num == "number" && num >= 0) {
+    for(i=0;i<num;i++) {
+      for(j=0;j<=i;j++) {
+        print += "#"
+      } print += "\n"
+    }
+  } else {
+    print += "Parameter harus angka dan positif!"
+  }
+ return print
+}
+
+console.log(triangle(5));
